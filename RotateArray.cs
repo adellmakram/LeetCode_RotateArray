8cfef12f3f13b7bee3a082a796e48fd75e21f77e
Adel Makram <133242438+adellmakram@users.noreply.github.com>
2024-03-14 13:37:13 +0200

    Add files via upload

---
+++ RotateArray.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode
+{
+    public static class Solution
+    {
+        public static void Rotate(int[] nums, int k) 
+        {
+            while(nums.Length < k)
+                k -= nums.Length;
+
+            if (k > 0)
+            {
+                int[] ints = new int[nums.Length];
+                k = nums.Length - k;
+
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    ints[i] = nums[k + i];
+                    if ((k + i) == (nums.Length - 1))
+                        k = -(i + 1);
+                }
+
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    nums[i] = ints[i];
+                }
+            }
+        }
+    }
+}
